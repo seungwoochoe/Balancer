6f1cbc16eac52d419016ac702dd09990faccb3ba
Seungwoo Choe <seungwoochoes@gmail.com>
2021-11-11 00:08:42 +0900

    Rename some variables and add resetWeight function

--- containers/Shuffle.js
+++ containers/Shuffle.js
@@ -1,100 +1,113 @@
 // import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const fakeMusicList = [
-	{
-		"title": "1",
-		"artist": "AKMU",
-		"weight": 100
-	},
-	{
-		"title": "2",
-		"artist": "LAUV",
-		"weight": 100
-	},
-	{
-		"title": "3",
-		"artist": "LAUV",
-		"weight": 100
-	},
-	{
-		"title": "4",
-		"artist": "Mark Zuckerberg",
-		"weight": 100
-	},
-	{
-		"title": "5",
-		"artist": "Meta",
-		"weight": 100
-	},
+  {
+    "title": "1",
+    "artist": "AKMU",
+    "weight": 100
+  },
+  {
+    "title": "2",
+    "artist": "LAUV",
+    "weight": 100
+  },
+  {
+    "title": "3",
+    "artist": "LAUV",
+    "weight": 100
+  },
+  {
+    "title": "4",
+    "artist": "Mark Zuckerberg",
+    "weight": 100
+  },
+  {
+    "title": "5",
+    "artist": "Meta",
+    "weight": 100
+  },
 ];
 
-const fakeUserActivity = [
-	{
-		"title": "3",
-		"action": "like"
-	},
-	{
-		"title": "4",
-		"action": "skip"
-	},
-	{
-		"title": "4",
-		"action": "skip"
-	},
-	{
-		"title": "4",
-		"action": "skip"
-	},
-	{
-		"title": "5",
-		"action": "skip"
-	},
-	{
-		"title": "5",
-		"action": "like"
-	}
-]
+let fakeuserActionList = [
+  {
+    "title": "3",
+    "action": "like"
+  },
+  {
+    "title": "4",
+    "action": "skip"
+  },
+  {
+    "title": "4",
+    "action": "skip"
+  },
+  {
+    "title": "4",
+    "action": "skip"
+  },
+  {
+    "title": "5",
+    "action": "skip"
+  },
+  {
+    "title": "5",
+    "action": "like"
+  }
+];
 
-const getMusicList = async () => {
-	try {
-		const musicListJsonString = await AsyncStorage.getItem('musicList');
-		return musicList = await JSON.parse(musicListJsonString);
-	} catch (err) {
-		console.log(err);
-	}
+const loadMusicList = async () => {
+  try {
+    const musicListJsonString = await AsyncStorage.getItem('musicList');
+    return musicList = await JSON.parse(musicListJsonString);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-const getUserActivity = async () => {
-	try {
-		const userActivityJsonString = await AsyncStorage.getItem('userActivity');
-		return userActivity = await JSON.parse(userActivityJsonString);
-	} catch (err) {
-		console.log(err);
-	}
+const loadUserActionList = async () => {
+  try {
+    const userActionListJsonString = await AsyncStorage.getItem('userActionList');
+    return userActionList = await JSON.parse(userActionListJsonString);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const updateWeight_skipped = (music) => {
-	music.weight = music.weight*0.9;
-	return music;
+  music.weight = music.weight*0.9;
+  return music;
 };
 
 const updateWeight_liked = (music) => {
-	music.weight = music.weight*1.1;
-	return music;
+  music.weight = music.weight*1.1;
+  return music;
+};
+
+const resetWeight = (musicList) => {
+  musicList.forEach(music => {
+    music.weight = 100;
+  })
 }
 
-// const musicList = getMusicList();
-// const userActivity = getUserActivity();
+const calculateWeight = (musicList, userActionList) => {
+  userActionList.forEach(element => {
+    let targetMusic = musicList.find(music => music.title === element.title);
 
-fakeUserActivity.forEach(element => {
-	let target = fakeMusicList.find(music => music.title === element.title);
+    if (element.action === "skip") {
+      targetMusic = updateWeight_skipped(targetMusic);
+    } else {
+      targetMusic = updateWeight_liked(targetMusic);
+    }
+  });
+  return musicList;
+};
 
-	if (element.action === "skip") {
-		target = updateWeight_skipped(target);
-	} else {
-		target = updateWeight_liked(target);
-	}
-});
 
-console.log(fakeUserActivity);
+
+// const musicList = loadMusicList();
+// let userActionList = loaduserActionList();
+
+calculateWeight(fakeMusicList, fakeuserActionList);
+
+console.log(fakeuserActionList);
 console.log(fakeMusicList);
