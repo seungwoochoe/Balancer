4dddde1d7acd49bcebacdc47d5cccea8f0d4e22e
Hannah54 <93320312+Hannah54@users.noreply.github.com>
2021-12-07 16:47:31 +0900

    Update data.js

--- models/data.js
+++ models/data.js
@@ -30,64 +30,95 @@ const songs = [
         id : "5"
     },
     {
-        title : "Complex",
-        artist: "ZionT",
-        image: require("../assets/artwork/ziont.jpeg"),
-        id : "6"
+	     title : "2 minutes more",
+	     artist: "Phao-Kaiz",
+         image: require("../assets/artwork/Pháo.jpg"),
+         id : "6"
     },
     {
-        title : "아무노래",
-        artist: "Zico",
-        image: require("../assets/artwork/anysong.jpg"),
-        id : "7"
+ 	     title : "Happy life",
+	     artist: "Fredji",
+         image: require("../assets/artwork/bruce-mars-AndE50aaHn4-unsplash.jpg"),
+         id : "7"
     },
     {
-        title : "Lilac",
-        artist: "IU",
-        image: require("../assets/artwork/lilac.jpg"),
-        id : "8"
+         title : "Classic",
+	     artist: "JoaKim Karud",
+         image: require("../assets/artwork/rovshan-allahverdiyev-II6-iFUU4z4-unsplash.jpg"),
+	     id : "8"
     },
     {
-        title : "Complex",
-        artist: "ZionT",
-        image: require("../assets/artwork/ziont.jpeg"),
-        id : "9"
+	     title : "Ice",
+	     artist: "Altimus",
+	     image: require("../assets/artwork/ian-dooley-TLD6iCOlyb0-unsplash.jpg"),
+	     id : "9"
+	},
+    {
+	     title : "Everyday",
+	     artist: "Jazz",
+	     image: require("../assets/artwork/jakub-david-ypPVnKK72NQ-unsplash.jpg"),
+	     id : "10"
     },
     {
-        title : "아무노래",
-        artist: "Zico",
-        image: require("../assets/artwork/anysong.jpg"),
-        id : "10"
+	     title : "TFF",
+	     artist: "Fly Away",
+	     image: require("../assets/artwork/hiva-sharifi-96TecBlEqR4-unsplash.jpg"),
+	     id : "11"
+	},
+    {
+	     title : "Sign",
+	     artist: "Deamn",
+	     image: require("../assets/artwork/paule-knete-qKKjDFPCts8-unsplash.jpg"),
+	     id : "12"
+	},
+    {
+	     title : "Horoes Tonight ( feat. Johnning)",
+	     artist: "Janji",
+	     image: require("../assets/artwork/alexandr-bormotin-FuHKcKoPl0c-unsplash.jpg"),
+	     id : "13"
+	},
+    {
+	     title : "Acting",
+	     artist: "Melodiesinfonie",
+	     image: require("../assets/artwork/gabriel-matula-ym5smgBeJwg-unsplash.jpg "),
+	     id : "14"
+	},
+    {
+	     title : "Bigcuit",
+	     artist: "Lukrembo",
+	     image: require("../assets/artwork/rai-vidanes-tVy3x2Vu_z4-unsplash.jpg"),
+	     id : "15"
+	},
+    {
+	     title : "Bread",
+	     artist: "Lukrembo",
+	     image: require("../assets/artwork/.jpeg"),
+	     id : "16"
     },
     {
-        title : "Lilac",
-        artist: "IU",
-        image: require("../assets/artwork/lilac.jpg"),
-        id : "11"
+	     title : "New Day",
+	     artist: "Ikson",
+	     image: require("../assets/artwork/.jpeg"),
+	     id : "17"
     },
     {
-        title : "Complex",
-        artist: "ZionT",
-        image: require("../assets/artwork/ziont.jpeg"),
-        id : "12"
+	     title : "Rainbow",
+	     artist: "Lichu",
+	     image: require("../assets/artwork/.jpeg"),
+	     id : "18"
     },
     {
-        title : "아무노래",
-        artist: "Zico",
-        image: require("../assets/artwork/anysong.jpg"),
-        id : "13"
+	     title : "Flying High",
+	     artist: "Fredji",
+	     image: require("../assets/artwork/.jpeg"),
+	     id : "19"
     },
     {
-        title : "Lilac",
-        artist: "IU",
-        image: require("../assets/artwork/lilac.jpg"),
-        id : "14"
-    },
-    {
-        title : "Complex",
-        artist: "ZionT",
-        image: require("../assets/artwork/ziont.jpeg"),
-        id : "15"
-    },
+	     title : "Stereotypy",
+	     artist: "song by StayC, piano by Doopiano",
+	     image: require("../assets/artwork/.jpeg"),
+	     id : "20"
+	},
+
 ]
 export default songs;
