34584ad1bb156078cfac53a1dbf65fe1b5a4751a
신정한 <soccersinjun@gmail.com>
2021-12-09 18:27:23 +0900

    dd

--- models/data.js
+++ models/data.js
@@ -6,98 +6,98 @@ const songs = [{
         id: "6"
     },
     {
-        url: require('../assets/songs/7.mp3'),
+        uri: require('../assets/songs/7.mp3'),
         title: "Happy life",
         artist: "Fredji",
         image: require("../assets/artworks/pexels-pixabay-160722.jpg"),
         id: "7"
     },
     {
-        url: require('../assets/songs/8.mp3'),
+        uri: require('../assets/songs/8.mp3'),
         title: "Classic",
         artist: "JoaKim Karud",
         image: require("../assets/artworks/rovshan-allahverdiyev-II6-iFUU4z4-unsplash.jpg"),
         id: "8"
     },
     {
-        url: require('../assets/songs/9.mp3'),
+        uri: require('../assets/songs/9.mp3'),
         title: "Inspire",
         artist: "Benjamin Tissot",
         image: require("../assets/artworks/pexels-eberhard-grossgasteiger-2437286.jpg"),
         id: "9"
     },
     {
-        url: require('../assets/songs/bensound-happiness.mp3'),
+        uri: require('../assets/songs/bensound-happiness.mp3'),
         title: "Hapiness",
         artist: "Bensound ",
         image: require("../assets/artworks/pexels-nikolaeva-nastia-10457501.jpg"),
         id: "10"
     },
     {
-        url: require('../assets/songs/bensound-dreams.mp3'),
+        uri: require('../assets/songs/bensound-dreams.mp3'),
         title: "Dreams",
         artist: "Benjamin Tissot",
         image: require("../assets/artworks/pexels-josh-sorenson-737552.jpg"),
         id: "11"
     },
     {
-        url: require('../assets/songs/bensound-erf.mp3'),
+        uri: require('../assets/songs/bensound-erf.mp3'),
         title: "ERF",
         artist: "Benjamin Tissot",
         image: require("../assets/artworks/pexels-jill-burrow-6858673.jpg"),
         id: "12"
     },
     {
-        url: require('../assets/songs/13.mp3'),
+        uri: require('../assets/songs/13.mp3'),
         title: "Heroes Tonight (feat. Johnning)",
         artist: "Janji",
         image: require("../assets/artworks/pexels-gelatin-6357186.jpg"),
         id: "13"
     },
     {
-        url: require('../assets/songs/bensound-memories.mp3'),
+        uri: require('../assets/songs/bensound-memories.mp3'),
         title: "Memories",
         artist: "Benjamin Tissot",
         image: require("../assets/artworks/pexels-aleksey-kuprikov-3493777.jpg"),
         id: "14"
     },
     {
-        url: require('../assets/songs/bensound-creativeminds.mp3'),
+        uri: require('../assets/songs/bensound-creativeminds.mp3'),
         title: "Creative Minds",
         artist: "Benjamin Tissot",
         image: require("../assets/artworks/pexels-moussa-idrissi-3694341.jpg"),
         id: "15"
     },
     {
-        url: require('../assets/songs/16.mp3'),
+        uri: require('../assets/songs/16.mp3'),
         title: "Bread",
         artist: "Lukrembo",
         image: require("../assets/artworks/pexels-mark-neal-3115006.jpg"),
         id: "16"
     },
     {
-        url: require('../assets/songs/17.mp3'),
+        uri: require('../assets/songs/17.mp3'),
         title: "A New Day",
         artist: "Ikson",
         image: require("../assets/artworks/pexels-samuel-theo-manat-silitonga-848573.jpg"),
         id: "17"
     },
     {
-        url: require('../assets/songs/18.mp3'),
+        uri: require('../assets/songs/18.mp3'),
         title: "Rainbow",
         artist: "Lichu",
         image: require("../assets/artworks/pexels-simon-berger-680562.jpg"),
         id: "18"
     },
     {
-        url: require('../assets/songs/19.mp3'),
+        uri: require('../assets/songs/19.mp3'),
         title: "Flying High",
         artist: "Fredji",
         image: require("../assets/artworks/pexels-ricardo-esquivel-1683492.jpg"),
         id: "19"
     },
     {
-        url: require('../assets/songs/20.mp3'),
+        uri: require('../assets/songs/20.mp3'),
         title: "Stereotypy",
         artist: "song by StayC, piano by Doopiano",
         image: require("../assets/artworks/pexels-josh-sorenson-831889.jpg"),
