0de712588493fa9282e207c9c8ccbd78137c4d5b
신정한 <soccersinjun@gmail.com>
2021-12-09 18:20:30 +0900

    dd

---
+++ component/musicNow.js
@@ -0,0 +1,16 @@
+import { Audio } from 'expo-av';
+
+
+export const SoundObj = new Audio.Sound();
+
+let songNow = {
+    title: "아무노래",
+    artist: "Zico",
+    image: require("../assets/artwork/anysong.jpg"),
+    id: "1",
+    uri: require('../assets/songs/1.mp3'),
+    duration: 172,
+    isPlayin: true
+}
+
+export default songNow;
